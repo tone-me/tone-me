f3ae041d044076730e32daca7100f553ad283aba
Anshul Gupta <hi.anshulg@gmail.com>
2023-11-28 17:45:57 -0500

    amazing landing page

---
+++ app/(default)/page.tsx
@@ -0,0 +1,81 @@
+// 'use client'
+// import RecordingView from "./components/WorkingRecordingView"
+// import './globals.css'
+// import {useState} from 'react'
+
+// // dependencies: {
+// //   "@testing-library/jest-dom": "^5.17.0",
+// //   "@testing-library/react": "^13.4.0",
+// //   "@testing-library/user-event": "^13.5.0",
+// //   "react": "^18.2.0",
+// //   "react-dom": "^18.2.0",
+// //   "react-icons": "^4.11.0",
+// //   "react-router-dom": "^6.16.0",
+// //   "react-scripts": "5.0.1",
+// //   "styled-components": "^5.3.10",
+// //   "web-vitals": "^2.1.4"
+// // }
+
+
+// import {
+//     BrowserRouter as Router,
+//     Routes,
+//     Route,
+// } from "react-router-dom";
+
+// const titleStyle = {
+//   marginTop:"30px",
+//   marginBottom: "15px",
+//   fontSize: "60px",
+//   color: "#bc6c25",
+//   fontFamily: "Trebuchet MS"
+// }
+
+// const subtitleStyle = {
+//   marginTop: "12px",
+//   fontSize: "30px",
+//   fontFamily: "Trebuchet MS",
+//   color: "#d4a373"
+// }
+
+
+// export default function Home() {
+//   return (
+//     <main className="flex min-h-screen flex-col items-center bg-gray-10">
+//       <div style={{color:"black"}}>
+//         <h1 style={titleStyle}>
+//           *✧･ﾟ:* Tone-Me ✧･ﾟ: *✧
+//           </h1>
+//         <p style={subtitleStyle}>
+//           Your Personal Chinese Mandarin Tone & Pronunciation Assistant 
+//         </p>
+//         <RecordingView/>
+//       </div>
+//     </main>
+//   )
+//   }
+
+
+export const metadata = {
+  title: 'Home - Tone.me',
+  description: 'Page description',
+}
+
+import Header from '../components/header'
+import Hero from '../components/hero'
+import Features from '../components/features'
+import FeaturesBlocks from '../components/features-blocks'
+import Footer from '../components/footer'
+
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Features />
+      <FeaturesBlocks />
+      <Footer />
+    </>
+  )
+}
---
+++ app/components/footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        {/* Bottom area */}
+        <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
+          {/* Copyrights note */}
+          <div className="text-sm text-gray-600 mr-4">&copy; Tone-Me, by Chris, Daria, Anshul, Riddhi, and Kat</div>
+        </div>
+      </div>
+    </footer>
+  )
+}
