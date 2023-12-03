a4cce492732b7b6a0204c910b94ba81bc26b826f
Chris Ge <chrisge@dhcp-10-29-237-0.dyn.MIT.EDU>
2023-12-03 11:32:09 -0500

    fixed some tailwind.css issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      extend: {
+        textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+        // other variants...
+      },
     },
   },
   plugins: [],
