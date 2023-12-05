40c5167e4d14ff0bc0abfb4c143d6f2b5795af79
Chris Ge <chrisge@dhcp-10-29-237-0.dyn.MIT.EDU>
2023-12-04 21:25:24 -0500

    trying a slightly different app.run for the render

--- app/app.py
+++ app/app.py
@@ -131,4 +131,5 @@ if __name__ == "__main__":
     path_to_audio = "./recording.wav"
     # print(sample_audio)
     # print(evaluate_model(audio=audio, rate=rate, model=model, feature_extractor=feature_extractor))
-    app.run(debug=True, host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
