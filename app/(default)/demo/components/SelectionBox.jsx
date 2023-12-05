16a3ddb7cd7346ae6d3bc3e29198290e909fa727
Chris Ge <chrisge@dhcp-10-29-237-0.dyn.MIT.EDU>
2023-12-04 20:24:04 -0500

    not sure what the changes are but I need to git pull

--- app/(default)/demo/components/SelectionBox.jsx
+++ app/(default)/demo/components/SelectionBox.jsx
@@ -6,7 +6,7 @@ const SelectionBox = ( {tonestring, setTonestring, setInputText} ) => {
         const formData = new FormData(e.currentTarget);
         setTonestring(formData.get("tone-input"));
         try {
-            const response = await fetch("https://tone-me.onrender.com/fetch_text", {
+            const response = await fetch("http://127.0.0.1:5000/fetch_text", {
               method: "POST",
               mode: "cors",
               headers: {
