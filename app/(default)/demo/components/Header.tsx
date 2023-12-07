cd3d2a4257f2efcb362ef4fad9a8e1609390fdbf
dariakryvosheieva <144752536+dariakryvosheieva@users.noreply.github.com>
2023-12-07 14:48:09 -0500

    minor changes to the frontend

--- app/(default)/demo/components/Header.tsx
+++ app/(default)/demo/components/Header.tsx
@@ -35,9 +35,9 @@ const Header = () => {
         </svg>
       </div>
       {/* Hero content */}
-      <div className="pt-32 relative w-screen mx-auto flex flex-row container">
+      <div className="pt-32 relative w-screen mx-auto flex flex-row items-center justify-center container">
         {/* Section header */}
-        <div className="text-left text-3xl mb-2 bg-gray-900 p-4 h3 text-white rounded-lg">
+        <div className="text-3xl mb-2 bg-gray-900 p-4 h3 text-white rounded-lg">
           Tone-Me{" "}
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
             Demo
@@ -48,14 +48,14 @@ const Header = () => {
         data-aos="zoom-y-out"
         className="mt-10 relative w-screen mx-auto flex flex-row items-center justify-center container text-xl py-10 text-left bg-gray-200 backdrop-blur-lg rounded-xl m-0"
       >
-        <ol className="list-decimal space-y-3 list-inside">
+        <ol className="px-10 list-decimal space-y-3 list-inside">
           <li>
             Submit the Mandarin Chinese sentence or phrase you want to say on
             the left below.
           </li>
-          <ol style={{ listStyleType: "lower-alpha", marginLeft: "2em" }}>
+          <ol style={{ listStyleType: "square", marginLeft: "2em" }}>
             <li>
-              For higher accuracy, you may also submit intended tones
+              For higher accuracy, you may also submit the intended tones
               (especially useful for ambiguous characters).
             </li>
           </ol>
@@ -68,16 +68,12 @@ const Header = () => {
             in the recording.
           </li>
           <ol
-            style={{ listStyleType: "lower-alpha", marginLeft: "2em" }}
+            style={{ listStyleType: "square", marginLeft: "2em" }}
             className="space-y-1"
           >
             <li>
-              Press the grey "Start" button that appears. Every time are between
-              two syllables, press "Mark syllable".
-            </li>
-            <li>
-              Once you're done, press "Stop". Results should aggregate on a
-              table after processing the input data.
+              Press the grey "Start" button that appears. Every time you are between
+              two syllables, press "Mark syllable". Once you're done, press "Stop".
             </li>
           </ol>
         </ol>
