2150f8f09d4a0fc61935c04ebb3b2d5e7729b50c
Anshul Gupta <hi.anshulg@gmail.com>
2023-12-09 23:21:36 -0500

    cleaning

--- app/(default)/demo/components/Header.tsx
+++ app/(default)/demo/components/Header.tsx
@@ -5,35 +5,6 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="">
-      {/* <div
-        className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1"
-        aria-hidden="true"
-      >
-        <svg
-          width="1360"
-          height="800"
-          viewBox="0 0 1360 578"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <defs>
-            <linearGradient
-              x1="50%"
-              y1="0%"
-              x2="50%"
-              y2="100%"
-              id="illustration-01"
-            >
-              <stop stopColor="#FFF" offset="0%" />
-              <stop stopColor="#EAEAEA" offset="77.402%" />
-              <stop stopColor="#DFDFDF" offset="100%" />
-            </linearGradient>
-          </defs>
-          <g fill="url(#illustration-01)" fillRule="evenodd">
-            <circle cx="1232" cy="128" r="128" />
-            <circle cx="155" cy="443" r="64" />
-          </g>
-        </svg>
-      </div> */}
       {/* Hero content */}
       <div className="pt-32 relative w-screen mx-auto flex flex-row items-center justify-center container">
         {/* Section header */}
@@ -87,8 +58,5 @@ const Header = () => {
 export default Header;
 
 {
-  /* <h1 style={titleStyle}>*✧･ﾟ:* Tone-Me ✧･ﾟ: *✧</h1>
-        <p style={subtitleStyle}>
-          Your Personal Chinese Mandarin Tone & Pronunciation Assistant
-        </p> */
+
 }
--- app/(default)/demo/components/SelectionBox.jsx
+++ app/(default)/demo/components/SelectionBox.jsx
@@ -94,27 +94,6 @@ const SelectionBox = ( {setTonestring, setInputText} ) => {
                 </div>
             </div>
         </section>  
-            {/* <div className="w-full max-w-xs">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit = {onSubmit}>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="text-input">
-                        Sentence you want recorded
-                    </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="text-input" id="text-input" type="text-input" placeholder="中国"/>
-                </div>
-                    <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="tone-input">
-                        Input the tones for your sentence for higher accuracy (optional)
-                    </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name = "tone-input" id="tone-input" type="tone-input" placeholder="1 2"/>
-                </div>
-                <div className="flex items-center justify-between">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                    Save
-                </button>
-                </div>
-            </form>
-            </div> */}
         </>
     );
 };
